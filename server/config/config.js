34e98dc92472045d09f99a12feb95bfdbb0193c8
SebastianBraun2013 <Sebastian.braun@blackburn.edu>
2020-02-12 21:09:56 -0600

    Update

--- server/config/config.js
+++
@@ -1,8 +0,0 @@
-'use strict'
-const express = require('express')
-const app = express()
-const path = require('path')
-
-app.use(express.static(path.join(__dirname, './../../dist/client/user.html')))
-
-module.exports = app
--- server/index.js
+++ server/index.js
@@ -1,29 +1,8 @@
 'use strict'
 const http = require('http')
-const app = require('./config/config')
+const app = require('./config')
 const Server = http.Server(app)
 const PORT = process.env.PORT || 8000
 const io = require('socket.io')(Server)
 
-Server.listen(PORT, () => console.log('Game server running on:', PORT))
-const players = {}
-
-app.get('/', function(req, res){
-    res.sendFile(__dirname + '/user.html');
-});
-io.on('connection', socket => {
-    socket.emit('connect', { hello: 'world'});
-    // When a player connects
-    socket.on('confirm', function (data){ 
-        console.log(data);
-    })
-    console.log('Connection made')
-    
-
-    socket.on('disconnect', state => {
-        delete players[socket.id]
-        //io.emit('update-players', players)
-    })
-
-
-})
+Server.listen(PORT, () => console.log('Server running on:', PORT))
